3bda0c67266760d8905e266485cc8cfa667639fe
White Rabbit Git <94390734+Harloy@users.noreply.github.com>
2025-04-01 08:17:13 +0300

    Update Prac6.kt

--- src/Prac6.kt
+++ src/Prac6.kt
@@ -1,9 +1,13 @@
 fun prac6() {
     print("Введите ваше имя: ")
-    val name = readLine()!!
-
+    val name = readLine()?.trim()
+    
     print("Введите ваш возраст: ")
-    val age = readLine()!!.toInt()
-
-    println("Привет, $name! Тебе уже $age")
+    val age = readLine()?.toIntOrNull()
+    
+    if (name.isNullOrBlank() || age == null) {
+        println("Ошибка ввода.")
+    } else {
+        println("Привет, $name! Тебе уже $age")
+    }
 }
