480677be521d8c1f56de7895e963ab4cb266dac5
White Rabbit Git <94390734+Harloy@users.noreply.github.com>
2025-04-01 08:16:04 +0300

    Update Prac5.kt

--- src/Prac5.kt
+++ src/Prac5.kt
@@ -1,8 +1,11 @@
 fun prac5() {
     print("Введите число: ")
-    val n = readLine()!!.toInt()
-
-    val result = n * 10 + n * 2
-
-    println("Результат: $result")
+    val input = readLine()?.toIntOrNull()
+    
+    if (input != null) {
+        val result = "$input${input * 2}${input * 3}".toInt()
+        println("Результат: $result")
+    } else {
+        println("Ошибка: введите корректное число.")
+    }
 }
